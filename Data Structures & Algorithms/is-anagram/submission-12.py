6507adc87df8751adf4806871ebc4c67b6e3a171
Parth Tuteja <88156096+parth2teja23@users.noreply.github.com>
2026-04-11 12:18:18 +0530

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        visited = [0] * 26
+
+        for ch in s:
+            visited[ord(ch) - ord('a')] += 1
+        
+        for ch in t:
+            visited[ord(ch) - ord('a')] -= 1
+        
+        for i in visited:
+            if i != 0:
+                return False
+        return True
